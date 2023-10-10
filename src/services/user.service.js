92e88c759a2a4cd31b8e177efe465034eb524c8e
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2023-10-10 15:21:45 +0530

    env

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,54 +1,33 @@
 const { User } = require("../models");
-
-/**
- * Create user
- */
+// Create user service
 const create_user = async (reqBody) => {
   return User.create(reqBody);
 };
-
-/**
- * Get user list
- */
+// Get user list service
 const get_user_list = async () => {
   return User.find();
 };
-
-/**
- * Get user by email
- */
+// Get user by email service
 const get_user_by_email = async (email) => {
   return User.findOne({ email });
 };
-
-/**
- * Get user details by id
- */
+// Get user details by id service
 const get_user_by_id = async (user_id) => {
   return User.findById(user_id);
 };
-
-/**
- * user details update by id
- */
+// User details update by id
 const update_details = async (userId, updateBody) => {
   return User.findByIdAndUpdate(userId, { $set: updateBody });
 };
-
-/**
- * Delete user
- */
+// Delete user Service
 const delete_user = async (userId) => {
   return User.findByIdAndDelete(userId);
 };
-
-/**
- * Update user
- */
+// Update user controller
 const update_user = async (userId,reqbody) => {
   return User.findByIdAndUpdate(userId,{$set:reqbody});
 };
-
+// Service object export
 module.exports = {
   create_user,
   get_user_list,
